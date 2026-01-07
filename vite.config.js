d2c2b5bd6c0aa10f4dd31ae1fc2923f58c8b193c
caitann <caitlinann1104@gmail.com>
2026-01-07 14:10:29 +0530

    build for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   base: '/C_ann-profile/',
   plugins: [react()],
-  base: '/proj10/',
+  plugins: [react()],
 })
